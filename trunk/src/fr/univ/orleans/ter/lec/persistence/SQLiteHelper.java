14006dbb852d1ad310b82866521992cf4927ebdb
adizere@gmail.com <adizere@gmail.com@96e12977-be87-5430-da06-50e5b690d89a>
2012-02-22 01:08:25 +0000

    Added the trunk/ hierarchy of main files created by eclipse; Added basic persistence files & database XML model

---
+++ trunk/src/fr/univ/orleans/ter/lec/persistence/SQLiteHelper.java
@@ -0,0 +1,78 @@
+package fr.univ.orleans.ter.lec.persistence;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import fr.univ.orleans.ter.lec.persistence.sql.DbStructure;
+import fr.univ.orleans.ter.lec.persistence.sql.Table;
+
+/**
+ * Persistence Layer
+ * 
+ * SQLite Convenience Class - Helper needed for providing the database
+ * creation/update functionality.
+ * 
+ * Makes use of the DbStructure class in order to obtain information with regard
+ * to the SQL statements needed for various tables.
+ * 
+ * @author AdrianSeredinschi
+ * 
+ */
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+	// Some *constants*
+	private static final String DATABASE_NAME = "lec.db";
+	private static final int DATABASE_VERSION = 1;
+
+	private DbStructure dbStructure;
+
+	public SQLiteHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		dbStructure = new DbStructure(context);
+	}
+
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(getDbCreateStatement());
+	}
+
+	private String getDbCreateStatement() {
+		String createStmt = "";
+
+		ArrayList<Table> tables = dbStructure.getTables();
+		for (Table t : tables) {
+			createStmt += t.getCreateStatement();
+		}
+
+		Log.d(DATABASE_NAME, "Create Statement: \n " + createStmt + "\n");
+
+		return createStmt;
+	}
+
+	// no need to make this one public
+	private void onDrop(SQLiteDatabase database) {
+		database.execSQL(getDbDropStatement());
+	}
+
+	private String getDbDropStatement() {
+		String dropStmt = "";
+		ArrayList<Table> tables = dbStructure.getTables();
+		for (Table t : tables) {
+			dropStmt += t.getDropStatement();
+		}
+		Log.d(DATABASE_NAME, "Drop Statement: \n " + dropStmt + "\n");
+
+		return dropStmt;
+	}
+
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(SQLiteHelper.class.getName(), "Upgrading database from version "
+				+ oldVersion + " to " + newVersion
+				+ ", which will destroy all old data");
+
+		onDrop(db);
+		onCreate(db);
+	}
+}
